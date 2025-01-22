9d138e8bafff7156ce8235bf07b3bbf74ba3c6a6
jelle <jellewierenga36@gmail.com>
2025-01-22 22:20:22 +0100

    Update main.py om met database.py te functioneren. Haal de database info op uit database.py

--- src/main.py
+++ src/main.py
@@ -1,32 +1,28 @@
 import os
 import mysql.connector
 from dotenv import load_dotenv
+from database import make_table, get_connection
 
 load_dotenv()
 folder = r"C:\Users\jelle\OneDrive - Hogeschool Leiden\ifiift"
 
-mydb = mysql.connector.connect(
-    host = os.getenv("host"),
-    user = os.getenv("user"),
-    password = os.getenv("password"),
-    database = os.getenv("database")
-)
 
+mydb = get_connection()
 mycursor = mydb.cursor()
 
-mycursor.execute("CREATE DATABASE IF NOT EXISTS file_scanner")
-mycursor.execute("DROP TABLE IF EXISTS file_info")
-mycursor.execute("CREATE TABLE IF NOT EXISTS file_info (id INT AUTO_INCREMENT PRIMARY KEY, path VARCHAR(255), file_name VARCHAR(255))")
-
+make_table(mydb)
 
 for root, dirs, files in os.walk(folder):
     if len(files) > 0 and files != ["0"] and "." in files[0]:
         for file in files:
             if "_" not in file:
-                print(file)
+                path = r"\\?\\" + os.path.join(root, file)
+                size = os.stat(path).st_size
+                print(f"{file}, {size} bytes")
+                # print(type(size))
 
-                sql = "INSERT INTO file_info (path, file_name) VALUES (%s, %s)"
-                val = (root, file)
+                sql = "INSERT INTO file_info (path, file_name, file_size) VALUES (%s, %s, %s)"
+                val = (root, file, size)
                 mycursor.execute(sql, val)
 
 mydb.commit()
