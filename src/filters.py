2cf73825616863ff0f7217e089229dbf5f35e3d3
jelle <jellewierenga36@gmail.com>
2025-02-05 18:33:56 +0100

    Add file name filter

--- src/filters.py
+++ src/filters.py
@@ -18,14 +18,21 @@ mycursor = mydb.cursor()
 
 #get specific filetypes
 mycursor.execute("SELECT DISTINCT extension from file_info")
-unique = [row[0] for row in mycursor.fetchall()]
-parser.add_argument("-ft", "--filetype", help="Search for 1 or more specific file types", choices=unique, nargs="*", type=str)
+uniqueType = [row[0] for row in mycursor.fetchall()]
+parser.add_argument("-ft", "--filetype", help="Search for 1 or more specific file types", choices=uniqueType, nargs="*", type=str)
 args = parser.parse_args()
 
 # get filesize
 mycursor.execute("SELECT DISTINCT file_size from file_info")
-uniquesize = [row[0] for row in mycursor.fetchall()]
+uniqueSize = [row[0] for row in mycursor.fetchall()]
 filetypes = args.filetype
-parser.add_argument("-fs", "--filesize", help="Get all the file with a specific file type", choices=uniquesize, nargs=1, type=int)
+parser.add_argument("--size", help="Get all the file with a specific file type", choices=uniqueSize, nargs=1, type=int)
+args = parser.parse_args()
+
+# get files with a specific name
+mycursor.execute("SELECT DISTINCT file_name from file_info")
+uniqueName = [row[0] for row in mycursor.fetchall()]
+filetypes = args.filetype
+parser.add_argument("--name", help="get all the files with the enterd name", choices=uniqueName, nargs=1, type=str)
 args = parser.parse_args()
 
